58ee2552d98624eb89cf64d28e412a681ecd3a8d
anthony9105 <anthony.liscio@ontariotechu.net>
2022-08-24 20:36:54 -0400

    Player type selection and display additions
    Added player type selection (just visual) and attribute changes display

--- script.js
+++ script.js
@@ -13,12 +13,33 @@ document.getElementById('box').addEventListener('wheel', function(){
 
 */
 
+
+// change upgrade display colour based on if it is positive (green) or negative (red)
+function changeUpgradeOptionColour(i)
+{
+  if (document.getElementsByClassName('upgrade-option-default')[i].innerHTML > 0)
+  {
+    document.getElementsByClassName('upgrade-option-default')[i].style.color = 'green';
+  }
+  else if (document.getElementsByClassName('upgrade-option-default')[i].innerHTML == 0)
+  {
+    document.getElementsByClassName('upgrade-option-default')[i].style.color = 'lightgray';
+  }
+  else
+  {
+    document.getElementsByClassName('upgrade-option-default')[i].style.color = 'red';
+  }
+}
+
 // increase attributes
 var userPlusSelection = document.getElementsByClassName('plus');
 
 for(let i = 0; i < userPlusSelection.length; i++) {
     userPlusSelection[i].addEventListener("click", function() {
     document.getElementsByClassName('numeric')[i].innerHTML++;
+    document.getElementsByClassName('upgrade-option-default')[i].innerHTML++;
+
+    changeUpgradeOptionColour(i);
   })
 }
 
@@ -29,5 +50,8 @@ var userMinusSelection = document.getElementsByClassName('minus');
 for(let i = 0; i < userMinusSelection.length; i++) {
     userMinusSelection[i].addEventListener("click", function() {
     document.getElementsByClassName('numeric')[i].innerHTML--;
+    document.getElementsByClassName('upgrade-option-default')[i].innerHTML--;
+
+    changeUpgradeOptionColour(i);
   })
 }
