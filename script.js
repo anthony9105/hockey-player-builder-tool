f4700fafa95c73441fc13f96883d0f66a9232413
Anthony Liscio <anthony.liscio@ontariotechu.net>
2023-05-09 23:02:07 -0400

    max upgrade of +5 and max downgrade of 5 (-5).  Also more organizing and comments.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/**
+ * 2022-2023
+ * Anthony Liscio
+ */
+
 import {buildDataXMLFileName, allHeights} from "./constants.js";
 
 /**
@@ -6,7 +11,7 @@ import {buildDataXMLFileName, allHeights} from "./constants.js";
  * It fetches the data found in the build_data.xml file.
  * @param {string} buildName 
  */
-function setDefaultAttributes(buildName) {
+function setupNewBuild(buildName) {
 
   // fetching from the build_data.xml file
   fetch(buildDataXMLFileName).then(response => {
@@ -24,86 +29,115 @@ function setDefaultAttributes(buildName) {
       // if the name of the current build matches with the paramaer value, buildName
       if (nameValue === buildName) {
 
-        console.log(nameValue);
-        var htmlAttributeTableIndex = 0;
-
-        // get the default attributes of this build
-        var defaultAttributes = build.querySelector("DefaultAttributes");
-
-        // amount of child nodes in defaultAttributes (divided by 2 since I am not including all the "text" child nodes)
-        var amountOfAttributeSections = Math.floor(defaultAttributes.childNodes.length / 2);
-
-        // for loop for each necessary child node in defaultAttributes (not looping over the "text" nodes which is why j starts
-        // at 1 and increments by 2 until it reaches amountOfAttributeSections multiplied by 2)
-        for (var j = 1; j < amountOfAttributeSections * 2; j += 2) {
-
-          // get the current attribute section ("Technique", "Power", "Playstyle", "Tenacity", or "Tactics")
-          var attributeSection = defaultAttributes.childNodes[j];
-
-          // amount of child nodes in attributeSecton (divided by 2 since I am not including all the "text" child nodes)
-          var amountOfAttributesInSection = Math.floor(attributeSection.childNodes.length / 2);
-
-          // for loop for each necessary child node in attributeSection (not looping over the "text" nodes which is why i starts
-          // at 1 and increments by 2 until it reaches amountOfAttributesInSection multiplied by 2)
-          for (var i = 1; i < amountOfAttributesInSection * 2; i += 2) {
-
-            // set the value in the html table
-            document.getElementsByClassName('numeric')[htmlAttributeTableIndex].innerHTML = attributeSection.childNodes[i].textContent;
-
-            // increment the html attribute table index
-            htmlAttributeTableIndex++;
-          }
-        }
+        setDefaultAttributes(build, nameValue);
 
         // reset the heights
         resetHeights();
 
-        // set default height
-        var defaultHeight = build.querySelector("Height").querySelector("default").textContent;
-        document.getElementById('height').value = defaultHeight;
+        setBuildHeights(build);
 
-        // set default weight
-        var defaultWeight = build.querySelector("Weight").querySelector("default").textContent;
-        document.getElementById('weight').value = defaultWeight;
-
-        // minimum heights and weights
-        var minHeight = convertFeetAndInchesToCm(build.querySelector("Height").querySelector("minimum").textContent);
-        var maxHeight = convertFeetAndInchesToCm(build.querySelector("Height").querySelector("maximum").textContent);
-
-        var minWeight = build.querySelector("Weight").querySelector("minimum").textContent;
-        var maxWeight = build.querySelector("Weight").querySelector("maximum").textContent;
-
-        var weight = document.getElementById("weight");
-
-        weight.min = minWeight;
-        weight.max = maxWeight;
-
-        minWeight = convertFeetAndInchesToCm(minWeight);
-        maxWeight = convertFeetAndInchesToCm(maxWeight);
-
-        var heights = document.getElementById('height');
-
-        // for each height option
-        for (var i = 0; i < heights.length; i++) {
-          var currentHeight = convertFeetAndInchesToCm(heights[i].value);
-
-          // if the current height is less than the minimum height for the current build then remove it
-          // from the options
-          if (currentHeight < minHeight) {
-            heights[i].remove(i);
-            i--;
-          }
-          // if the current height is greater than the maximum height for the current build then remove it
-          // from the options
-          else if (currentHeight > maxHeight) {
-            heights[i].remove(i);
-            i--;
-          }
-        }
+        setBuildWeights(build);
 
       }
     }
   });
+}
+
+/**
+ * setDefaultAttributes function.
+ * Used to set all the default attributes for the specific build
+ * @param {node} build 
+ * @param {string} nameValue 
+ */
+function setDefaultAttributes(build, nameValue) {
+  console.log(nameValue);
+  var htmlAttributeTableIndex = 0;
+
+  // get the default attributes of this build
+  var defaultAttributes = build.querySelector("DefaultAttributes");
+
+  // amount of child nodes in defaultAttributes (divided by 2 since I am not including all the "text" child nodes)
+  var amountOfAttributeSections = Math.floor(defaultAttributes.childNodes.length / 2);
+
+  // for loop for each necessary child node in defaultAttributes (not looping over the "text" nodes which is why j starts
+  // at 1 and increments by 2 until it reaches amountOfAttributeSections multiplied by 2)
+  for (var j = 1; j < amountOfAttributeSections * 2; j += 2) {
+
+    // get the current attribute section ("Technique", "Power", "Playstyle", "Tenacity", or "Tactics")
+    var attributeSection = defaultAttributes.childNodes[j];
+
+    // amount of child nodes in attributeSecton (divided by 2 since I am not including all the "text" child nodes)
+    var amountOfAttributesInSection = Math.floor(attributeSection.childNodes.length / 2);
+
+    // for loop for each necessary child node in attributeSection (not looping over the "text" nodes which is why i starts
+    // at 1 and increments by 2 until it reaches amountOfAttributesInSection multiplied by 2)
+    for (var i = 1; i < amountOfAttributesInSection * 2; i += 2) {
+
+      // set the value in the html table
+      document.getElementsByClassName('numeric')[htmlAttributeTableIndex].innerHTML = attributeSection.childNodes[i].textContent;
+
+      // increment the html attribute table index
+      htmlAttributeTableIndex++;
+    }
+  }
+}
+
+/**
+ * setBuildHeights function.
+ * Used to set the default height for specific build, and only include the height options that are allowed
+ * for the specific build.  For example: a sniper can be 5'7, 5'8, 5'9, 5'10, 5'11, 6'0, or 6'1, while the 
+ * allowed heights for a power forward are 6'2, 6'3, 6'4, 6'5, 6'6, 6'7.
+ * @param {node} build 
+ */
+function setBuildHeights(build) {
+  // set default height
+  var defaultHeight = build.querySelector("Height").querySelector("default").textContent;
+  document.getElementById('height').value = defaultHeight;
+
+  // minimum and maximum heights
+  var minHeight = convertFeetAndInchesToCm(build.querySelector("Height").querySelector("minimum").textContent);
+  var maxHeight = convertFeetAndInchesToCm(build.querySelector("Height").querySelector("maximum").textContent);
+
+  var heights = document.getElementById('height');
+
+  // for each height option
+  for (var i = 0; i < heights.length; i++) {
+    var currentHeight = convertFeetAndInchesToCm(heights[i].value);
+
+    // if the current height is less than the minimum height for the current build then remove it
+    // from the options
+    if (currentHeight < minHeight) {
+      heights[i].remove(i);
+      i--;
+    }
+    // if the current height is greater than the maximum height for the current build then remove it
+    // from the options
+    else if (currentHeight > maxHeight) {
+      heights[i].remove(i);
+      i--;
+    }
+  }
+}
+
+/**
+ * setBuildWeights function.
+ * Used to set the default weight as well as the minimum and maximum values allowed for the
+ * specific build.
+ * @param {node} build 
+ */
+function setBuildWeights(build) {
+  // set default weight
+  var defaultWeight = build.querySelector("Weight").querySelector("default").textContent;
+  document.getElementById('weight').value = defaultWeight;
+
+  // minimum and maximum weights
+  var minWeight = build.querySelector("Weight").querySelector("minimum").textContent;
+  var maxWeight = build.querySelector("Weight").querySelector("maximum").textContent;
+
+  // set the minimum and maximum weights for the html input box 
+  var weight = document.getElementById("weight");
+  weight.min = minWeight;
+  weight.max = maxWeight;
 }
 
 /**
@@ -181,7 +215,7 @@ document.getElementById('confirm-type').addEventListener('click', function(){
   var text = e.options[e.selectedIndex].text;
 
   // set the default attributes to the html table
-  setDefaultAttributes(text);
+  setupNewBuild(text);
 
   // reset the upgrades
   resetUpgrades();
@@ -225,10 +259,14 @@ var userPlusSelection = document.getElementsByClassName('plus');
 
 for(let i = 0; i < userPlusSelection.length; i++) {
   userPlusSelection[i].addEventListener("click", function() {
-    document.getElementsByClassName('numeric')[i].innerHTML++;
-    document.getElementsByClassName('upgrade-option-default')[i].innerHTML++;
 
-    changeUpgradeOptionColour(i);
+    // +5 is the maximum upgrade
+    if (document.getElementsByClassName('numeric')[i].innerHTML < 5) {
+      document.getElementsByClassName('numeric')[i].innerHTML++;
+      document.getElementsByClassName('upgrade-option-default')[i].innerHTML++;
+      changeUpgradeOptionColour(i);
+    }
+
   })
 }
 
@@ -238,9 +276,13 @@ var userMinusSelection = document.getElementsByClassName('minus');
 
 for(let i = 0; i < userMinusSelection.length; i++) {
   userMinusSelection[i].addEventListener("click", function() {
-    document.getElementsByClassName('numeric')[i].innerHTML--;
-    document.getElementsByClassName('upgrade-option-default')[i].innerHTML--;
 
-    changeUpgradeOptionColour(i);
+    // the maximum downgrade is 5 (-5)
+    if (document.getElementsByClassName('numeric')[i].innerHTML > -5) {
+      document.getElementsByClassName('numeric')[i].innerHTML--;
+      document.getElementsByClassName('upgrade-option-default')[i].innerHTML--;
+  
+      changeUpgradeOptionColour(i);
+    }
   })
 }
