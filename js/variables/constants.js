267a8714249d939f94419b5c60415cff319e3dd6
Anthony Liscio <anthony.liscio@ontariotechu.net>
2024-01-21 18:20:49 -0500

    update file path

--- js/variables/constants.js
+++ js/variables/constants.js
@@ -9,13 +9,16 @@
 /**
  * FILE PATHS
  */
+const websiteUrl = "https://anthony9105.github.io/hockey-player-builder-tool/";
+
 export const BUILD_DATA_XML = "../../build-information/build_data.xml";
 export const UPGRADE_POINTS_XML = "../../build-information/upgrade_points.xml";
 export const PHYS_UPGRADE_DOWNGRADE_XML = "../../build-information/physical_upgrade_downgrades.xml";
 export const BOOSTS_XML = "../../build-information/boosts.xml";
 export const MAIN_ABILITIES_XML = "../../build-information/main_abilities.xml";
 export const ABILITIES_XML = "../../build-information/abilities.xml";
-export const NHL_PLAYER_ATTRIBUTES_CSV = "../../player-information/realPlayerAttributes.csv"; 
+// export const NHL_PLAYER_ATTRIBUTES_CSV = "../../player-information/realPlayerAttributes.csv"; 
+export const NHL_PLAYER_ATTRIBUTES_CSV = websiteUrl + "/player-information/realPlayerAttributes.csv"; 
 
 
 /**
